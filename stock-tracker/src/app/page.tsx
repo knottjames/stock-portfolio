e7e3ede086dc416172c26e0b69d936c2d3fcf5d5
knottjames <jamesknott1340@gmail.com>
2023-08-25 14:12:09 +1200

    Add welcome message to home page

--- stock-tracker/src/app/page.tsx
+++ stock-tracker/src/app/page.tsx
@@ -1,13 +1,16 @@
 'use client'
-
+import { stepLabelClasses } from '@mui/material';
+import styles from './HomePage.module.css';
 
 export default function Home() {
   
   return (
 
 
-      <div>
-        hello
+      <div className = {styles.container}>
+        <h1 className = {styles.header}>Welcome to Stock Tracker!</h1>
+        To begin searching stocks, type a ticker symbol into the search bar above. E.g. "AAPL" for Apple.<br></br>
+        Currently, the app only supports US stocks and must be searched using their ticker symbol.<br></br>
        
       </div>
       
