7fb730b8a688304401599e47ef1afbf7edfa0c1b
knottjame <knottjame@ecs.vuw.ac.nz>
2023-09-26 21:46:13 +1300

    remove unused function

--- stock-tracker/src/app/portfolio/page.tsx
+++ stock-tracker/src/app/portfolio/page.tsx
@@ -4,6 +4,7 @@ import PortfolioGrid from '../components/portfolio/PortfolioGrid';
 import styles from './PortfolioPage.module.css';
 import Loading from './loading';
 
+
 export default function PortfolioPage() {
 
     return <div className={styles.container}>
@@ -14,3 +15,4 @@ export default function PortfolioPage() {
 
     </div>
 }
+
